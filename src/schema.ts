9d1f951ea82f85ad99633e31550f011c18e22c62
Michael Marchanka <michaskruzelka@protonmail.ch>
2021-01-08 12:50:12 +0300

    BUILD-DEBUG-001

--- src/schema.ts
+++ src/schema.ts
@@ -1,24 +1,25 @@
 import { loadFilesSync } from '@graphql-tools/load-files';
 import { mergeTypeDefs } from '@graphql-tools/merge';
+import { makeExecutableSchema } from 'apollo-server';
 import { DocumentNode, GraphQLSchema } from 'graphql';
 
-import { makeAugmentedSchema } from './neo4j-graphql';
+// import { makeAugmentedSchema } from './neo4j-graphql';
 import resolvers from './resolvers';
 
 const graphqlFiles: GraphQLSchema[] = loadFilesSync('./**/*.graphql');
 const typeDefs: DocumentNode = mergeTypeDefs(graphqlFiles);
 
-const schema = makeAugmentedSchema({
+const schema = makeExecutableSchema({
     typeDefs,
     resolvers,
-    config: {
-        query: {
-            exclude: ['Language', 'GroupedLanguages'],
-        },
-        mutation: {
-            exclude: ['Language', 'GroupedLanguages'],
-        },
-    },
+    // config: {
+    //     query: {
+    //         exclude: ['Language', 'GroupedLanguages'],
+    //     },
+    //     mutation: {
+    //         exclude: ['Language', 'GroupedLanguages'],
+    //     },
+    // },
 });
 
 export default schema;
