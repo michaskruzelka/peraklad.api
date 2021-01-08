51b79e1817ade1fbd17ab0ec134c2c545380d73e
Michael Marchanka <michaskruzelka@protonmail.ch>
2021-01-08 15:45:07 +0300

    BUILD-DEBUG-001: step 4

--- src/schema.ts
+++ src/schema.ts
@@ -1,8 +1,7 @@
 import { loadFilesSync } from '@graphql-tools/load-files';
 import { mergeTypeDefs } from '@graphql-tools/merge';
 import { DocumentNode, GraphQLSchema } from 'graphql';
-
-import { makeAugmentedSchema } from './neo4j-graphql';
+import { makeAugmentedSchema } from 'neo4j-graphql-js';
 import resolvers from './resolvers';
 
 const graphqlFiles: GraphQLSchema[] = loadFilesSync('./**/*.graphql');
