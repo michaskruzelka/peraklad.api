2af9cea4e3720cff37b1f1719a107f3d33548387
Michael Marchanka <michaskruzelka@protonmail.ch>
2021-02-25 13:56:27 +0300

    API-000: minor dev changes

--- src/services/graphql/validation.ts
+++ src/services/graphql/validation.ts
@@ -3,8 +3,11 @@ import { createComplexityLimitRule } from 'graphql-validation-complexity';
 
 const validationRules = [
     depthLimit(10),
-    createComplexityLimitRule(1500, {
+    createComplexityLimitRule(10000, {
         introspectionListFactor: 0.1,
+        // onCost: (cost: any) => {
+        //     console.log('query cost:', cost);
+        // },
     }),
 ];
 
--- src/services/neo4j/driver.ts
+++ src/services/neo4j/driver.ts
@@ -11,7 +11,7 @@ const driver = neo4j.driver(
     process.env.NEO4J_URI || 'bolt://localhost:7687',
     neo4j.auth.basic(
         process.env.NEO4J_USER || 'neo4j',
-        process.env.NEO4J_PASSWORD || 'neo4j'
+        process.env.NEO4J_PASSWORD || 'admin'
     ),
     {
         encrypted: isEncrypted()
